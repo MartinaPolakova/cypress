1dd02b9cf4a816e25af667b3bf908252e58f6ccd
Martina Polakova <martina.kuzelova.01@gmail.com>
2024-02-01 11:22:18 +0100

    01_done-selecting_and_type

--- cypress/e2e/01-first-flight.cy.js
+++ cypress/e2e/01-first-flight.cy.js
@@ -1,3 +1,32 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+    beforeEach(() => {
+        cy.visit('/jetsetter');
+      });
+
+      it('should have a form', () => {
+        cy.get('form').should('exist')
+      });
+
+      it('shoul have the words "Add Item"', () => {
+        cy.contains('Add Item');
+      });
+
+      it('should some new elements', () => {
+        cy.contains('Item');
+        cy.get('section ul li button');
+      });
+
+      it('select all buttons with string "Remote"', () => {
+        cy.get('ul') 
+        .find('li') 
+        .find('button') 
+        .filter(':contains("Remove")') 
+        .should('have.length.gt', 4); 
+      });
+
+      it('Type some text in input attribute', () => {
+        cy.get('[data-test="new-item-input"]').type('New text');
+      });
+});
